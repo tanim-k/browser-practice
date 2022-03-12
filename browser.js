a20df4caa77d297ed574dfb0b3fc5915443ad97c
tanim-k <tanimkhan877@gmail.com>
2022-03-13 00:58:52 +0600

    home works

---
+++ browser.js
@@ -0,0 +1,15 @@
+/* 
+home work:
+1. show output after 3.5 seconds
+2. take a number using prompt. add 200. then alert the result.
+3. do u want to see the href? if ok. then console.log href.
+4. purpose of cookies(5-7 lines)
+5. 3 differences between local storage and session storage:
+6. how does javascript run?(5-7 lines)
+7. list of asynchronous things in js/ browser
+8. what is event loop and how does it work?
+-----optional----
+9. make a simple website with two input fields and a button. product name and product price. by clicking on the button product will be added on local storage. you should be able to add multiple.
+10. display products on the website. if they esists on local storage. display from there
+11. go to stack over flow. create an account. spend 30 minutes on there.
+*/
